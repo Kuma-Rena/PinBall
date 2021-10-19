2dd128d71b8130040d1c8ac7dd4b90959fbc49ee
Kuma-Rena <kuki.short25@gmail.com>
2021-10-19 17:03:32 +0900

    Scoreの表示
    Lesson5で作成したPinBallをもとにスコアの表示ができるようにSprictを作成した

---
+++ Assets/ScoreSprict.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreSprict : MonoBehaviour
+{
+
+
+
+    private GameObject Scoretext ;
+    private int score = 0;
+
+    void Start()
+    {
+        
+
+        this.Scoretext = GameObject.Find("ScoreText");
+        
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        string tag = other.gameObject.tag;
+        if (tag == "SmallStarTag")
+        {
+            this.score += 1;
+        }
+        else if (tag == "LargeStarTag")
+        {
+            this.score += 3;
+        }
+        else if (tag == "SmallCloudTag")
+        {
+            this.score += 2;
+        }
+        else if (tag == "LargeCloudTag")
+        {
+            this.score += 5;
+        }
+        SetScore();
+    }
+
+    void SetScore()
+    {
+        this.Scoretext.GetComponent<Text>().text = "Score : " + score;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    
+}
